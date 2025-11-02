7d768604192da55cee678c8f3cf84ffcd703f783
vinu <ovinug@gmail.com>
2025-11-02 23:21:18 +0530

    cart product file rename done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar"
-import { Product } from "../components/product"
+import { Product } from "../components/Product"
 import { useCartContext } from "../reducerAndcontext/CartContext"
 
 const Cart = () => {
